30f58ebd57f917612555da1d2d99d389d471d8df
LethaboKgasi <lethabomonyuku@gmail.com>
2026-05-31 23:30:16 +0200

    API integration.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
 
   new Typewriter("#poem", {
-    strings: "Roses are red, violets are blue",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "e6013302dc471o4949fb0aet3fd8eff8";
+
+  let context = "You are a poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Remove any quotation marks at the beginning and the end of the poem." ;
+
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+
+  poemElement.classList.remove("hidden");
+
+  poemElement.innerHTML = `<div class="generating"> Generating a poem...</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
